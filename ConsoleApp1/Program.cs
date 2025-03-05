b929e5f2458c780e253edc379de7a22048e86872
whyisitos <ryskat12@gmail.com>
2025-03-05 13:40:07 +0200

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,29 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        List<int> numbers = new List<int>();
+        for (int i = 1; i <= 50; i++)
         {
+            numbers.Add(i);
+        }
+
+        PrintSpiral(numbers, 10);
+    }
+
+    static void PrintSpiral(List<int> list, int rowSize)
+    {
+        for (int i = 0; i < list.Count; i += rowSize)
+        {
+            var row = list.GetRange(i, Math.Min(rowSize, list.Count - i));
+            if ((i / rowSize) % 2 == 1)
+            {
+                row.Reverse(); // Кожен другий рядок реверсується
+            }
+            Console.WriteLine(string.Join(" ", row));
         }
     }
 }
